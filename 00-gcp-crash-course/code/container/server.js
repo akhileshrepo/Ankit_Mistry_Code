2911e360dae9c2554472438f18f127565069ff08
AKhilesh <kothapallyakhil123@gmail.com>
2023-05-17 17:28:34 +0530

    I made some chnages

--- 00-gcp-crash-course/code/container/server.js
+++ 00-gcp-crash-course/code/container/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> I need terraform from udemy </h1>");
+  response.end("<h1> I made some changes </h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
